1503728f100b66b3a4f49ecf805d1b9d5971e193
Igor Iric <iric_i@hotmail.com>
2024-02-11 18:07:33 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,24 @@
-
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-undef": "error",
+    "no-unused-vars": "warn",
+    "eqeqeq": ["error", "always"],
+    "curly": "error",
+    "no-multi-spaces": "error",
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+};
